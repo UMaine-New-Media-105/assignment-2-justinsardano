8adaf09aaaf3041eae75fe3cd6011a8ce1257833
justinsardano <124173790+justinsardano@users.noreply.github.com>
2023-02-21 08:20:30 -0500

    flag created using shapes
    https://editor.p5js.org/

---
+++ functioninitial/flag.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(220);
+  fill ("white")
+  rect (0,0,300,200)
+  fill ("red")
+  circle(150,100,100)
+  fill ("red")
+  rect (0,200,300,200)
+  fill ("white")
+  noStroke()
+  rect (135,255,25,80)
+  rect (108,282,80,25)
+}
